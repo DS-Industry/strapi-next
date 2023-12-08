913e0e9c78fdb813f20dba710987b8556f29c8e0
IgorZvezdilin <zvezdilin.work@mail.ru>
2023-12-08 15:46:27 +0300

    Update strapi db configuration

--- strapi-api/config/database.ts
+++ strapi-api/config/database.ts
@@ -4,6 +4,23 @@ export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
+    defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+          client: "postgres",
+          host: env("DATABASE_HOST", "localhost"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          username: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+          ssl: {
+            ca: env("DATABASE_CA", ""),
+          },
+        },
+      },
+    },
     mysql: {
       connection: {
         connectionString: env('DATABASE_URL'),
